9ad4185f3e935c663d6ff1e0f6ac3293a42b6484
Gerynho <gerynho@gmail.com>
2023-07-28 11:15:35 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD03Nmahkf6ryjGbAJeERREDA_Gn8IG2dY",
+    authDomain: "master-3de65.firebaseapp.com",
+    databaseURL: "https://master-3de65-default-rtdb.firebaseio.com",
+    projectId: "master-3de65",
+    storageBucket: "master-3de65.appspot.com",
+    messagingSenderId: "990079940856",
+    appId: "1:990079940856:web:d8aef75cf3649430fb630d",
+    measurementId: "G-XCDK2DLBKG"
+  }
+};
