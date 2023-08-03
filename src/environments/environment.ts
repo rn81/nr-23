876fe86618eba8bff64536b01546d657ddee37ab
Gerynho <gerynho@gmail.com>
2023-08-03 14:48:00 -0300

    Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyD03Nmahkf6ryjGbAJeERREDA_Gn8IG2dY",
-    authDomain: "master-3de65.firebaseapp.com",
-    databaseURL: "https://master-3de65-default-rtdb.firebaseio.com",
-    projectId: "master-3de65",
-    storageBucket: "master-3de65.appspot.com",
-    messagingSenderId: "990079940856",
-    appId: "1:990079940856:web:d8aef75cf3649430fb630d",
-    measurementId: "G-XCDK2DLBKG"
+    apiKey: "AIzaSyB_SHBYz3swlZ-yYq9GZTEnYg-4GiGHh7o",
+    authDomain: "master-ac55d.firebaseapp.com",
+    projectId: "master-ac55d",
+    storageBucket: "master-ac55d.appspot.com",
+    messagingSenderId: "248603782803",
+    appId: "1:248603782803:web:d1e1b7af2ff4089d3ba766",
+    measurementId: "G-TNL0X500D4"
   }
 };
