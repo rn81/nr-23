ae6107529edc16f38155a7f53397e7d4dd10098c
Rogério Nunes <107080897+rn81@users.noreply.github.com>
2023-08-05 20:00:50 -0300

    NR5 OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      const newLocal = 'nr04';
+      const newLocal = 'splash';
       this.router.navigateByUrl(newLocal);
     });
   }
