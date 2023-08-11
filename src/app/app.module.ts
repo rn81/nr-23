df4e08dbfa4ff46e24403364124417cca83e9739
Gerynho <gerynho@gmail.com>
2023-08-11 15:03:59 -0300

    Atualização das Normas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,13 +23,7 @@ import { SplashComponent } from './components/splash/splash.component';
 
 import { NrsComponent } from './components/nrs/nrs.component';
 
-import { Nr01Component } from './components/nrs/nr01/nr01.component';
-import { Nr02Component } from './components/nrs/nr02/nr02.component';
-import { Nr03Component } from './components/nrs/nr03/nr03.component';
-import { Nr04Component } from './components/nrs/nr04/nr04.component';
-import { Nr05Component } from './components/nrs/nr05/nr05.component';
-import { Nr06Component } from './components/nrs/nr06/nr06.component';
-import { Nr07Component } from './components/nrs/nr07/nr07.component';
+
 
 // routing
 import { AppRoutingModule } from './app-routing.module';
@@ -50,14 +44,8 @@ import { AuthenticationService} from "./shared/services/authentication.service";
     VerifyEmailComponent,
     SplashComponent,
     NrsComponent,
-    Nr01Component,  
-    Nr02Component,  
-    Nr03Component,     
-    Nr04Component,
-    Nr05Component,
-    Nr06Component,
-    Nr07Component,    
   ],
+
 
   imports: [
     BrowserModule,
